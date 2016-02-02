f4ee625df7c4a28fe83c8fcbd6f3b202443efcab
Andrew Couch <hi@andrewcou.ch>
2016-02-02 17:39:37 -0500

    void type alias

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = codegen = require('bindings')('codegen');
 
 codegen.type = {
 
+    void: codegen.getVoidTy(),
+
     i1: codegen.getIntTy(1),
     i8: codegen.getIntTy(8),
     i16: codegen.getIntTy(16),

46dc1ca07022530eb80479e37684c3f9653a29d0
Andrew Couch <hi@andrewcou.ch>
2016-11-25 09:49:50 -0500

    fix whitespace

--- test/petard/type/int_test.h
+++ test/petard/type/int_test.h
@@ -5,83 +5,83 @@
 class IntTypeTest : public CxxTest::TestSuite
 {
 public:
-		void testIntToString(void)
-		{
-				IntTypeHandle intType(32);
-				TS_ASSERT_EQUALS(intType.toString(), "i32");
-		}
+    void testIntToString(void)
+    {
+        IntTypeHandle intType(32);
+        TS_ASSERT_EQUALS(intType.toString(), "i32");
+    }
 
-		void testIntIsNotVoidType(void)
-		{
-				IntTypeHandle intType(32);
-				TS_ASSERT(!intType.isVoidType());
-		}
+    void testIntIsNotVoidType(void)
+    {
+        IntTypeHandle intType(32);
+        TS_ASSERT(!intType.isVoidType());
+    }
 
-		void testIntIsIntType(void)
-		{
-				IntTypeHandle intType(32);
-				TS_ASSERT(intType.isIntType())
-		}
+    void testIntIsIntType(void)
+    {
+        IntTypeHandle intType(32);
+        TS_ASSERT(intType.isIntType())
+    }
 
-		void testIntIsNotFloatType(void)
-		{
-				IntTypeHandle intType(32);
-				TS_ASSERT(!intType.isFloatType())
-		}
+    void testIntIsNotFloatType(void)
+    {
+        IntTypeHandle intType(32);
+        TS_ASSERT(!intType.isFloatType())
+    }
 
-		void testIntIsNotPointerType(void)
-		{
-				IntTypeHandle intType(32);
-				TS_ASSERT(!intType.isPointerType())
-		}
+    void testIntIsNotPointerType(void)
+    {
+        IntTypeHandle intType(32);
+        TS_ASSERT(!intType.isPointerType())
+    }
 
-		void testIntIsNotFunctionType(void)
-		{
-				IntTypeHandle intType(32);
-				TS_ASSERT(!intType.isFunctionType())
-		}
+    void testIntIsNotFunctionType(void)
+    {
+        IntTypeHandle intType(32);
+        TS_ASSERT(!intType.isFunctionType())
+    }
 
-		void testIntIsNotVectorType(void)
-		{
-				IntTypeHandle intType(32);
-				TS_ASSERT(!intType.isVectorType())
-		}
+    void testIntIsNotVectorType(void)
+    {
+        IntTypeHandle intType(32);
+        TS_ASSERT(!intType.isVectorType())
+    }
 
-		void testIntIsNotArrayType(void)
-		{
-				IntTypeHandle intType(32);
-				TS_ASSERT(!intType.isArrayType())
-		}
+    void testIntIsNotArrayType(void)
+    {
+        IntTypeHandle intType(32);
+        TS_ASSERT(!intType.isArrayType())
+    }
 
-		void testIntIsNotStructType(void)
-		{
-				IntTypeHandle intType(32);
-				TS_ASSERT(!intType.isStructType())
-		}
+    void testIntIsNotStructType(void)
+    {
+        IntTypeHandle intType(32);
+        TS_ASSERT(!intType.isStructType())
+    }
 
-		void testIntIsCompatibleWithIntOfSameWidth(void)
-		{
-				IntTypeHandle intType(32), anotherInt(32);
-				TS_ASSERT(intType.isCompatibleWith(&anotherInt));
-		}
+    void testIntIsCompatibleWithIntOfSameWidth(void)
+    {
+        IntTypeHandle intType(32), anotherInt(32);
+        TS_ASSERT(intType.isCompatibleWith(&anotherInt));
+    }
 
-		void testIntIsNotCompatibleWithIntOfDifferentWidth(void)
-		{
-				IntTypeHandle intType(32), largerInt(64);
-				TS_ASSERT(!intType.isCompatibleWith(&largerInt));
-		}
+    void testIntIsNotCompatibleWithIntOfDifferentWidth(void)
+    {
+        IntTypeHandle intType(32), largerInt(64);
+        TS_ASSERT(!intType.isCompatibleWith(&largerInt));
+    }
 
-		void testIntIsNotCompatibleWithOtherTypes(void)
-		{
-				IntTypeHandle intType(32);
-				VoidTypeHandle voidType;
-				FloatTypeHandle floatType(64);
-				TS_ASSERT(!intType.isCompatibleWith(&voidType));
-				TS_ASSERT(!intType.isCompatibleWith(&floatType));
-		}
+    void testIntIsNotCompatibleWithOtherTypes(void)
+    {
+        IntTypeHandle intType(32);
+        VoidTypeHandle voidType;
+        FloatTypeHandle floatType(64);
+        TS_ASSERT(!intType.isCompatibleWith(&voidType));
+        TS_ASSERT(!intType.isCompatibleWith(&floatType));
+    }
 
-		void testIntGetLLVMType(void)
-		{
-				TS_SKIP("TODO: LLVM tests");
-		}
+    void testIntGetLLVMType(void)
+    {
+        TS_SKIP("TODO: LLVM tests");
+    }
 };
--- test/petard/type/void_test.h
+++ test/petard/type/void_test.h
@@ -5,77 +5,77 @@
 class VoidTypeTest : public CxxTest::TestSuite
 {
 public:
-		void testVoidToString(void)
-		{
-				VoidTypeHandle voidType;
-				TS_ASSERT_EQUALS(voidType.toString(), "void");
-		}
+    void testVoidToString(void)
+    {
+        VoidTypeHandle voidType;
+        TS_ASSERT_EQUALS(voidType.toString(), "void");
+    }
 
-		void testVoidIsVoidType(void)
-		{
-				VoidTypeHandle voidType;
-				TS_ASSERT(voidType.isVoidType());
-		}
+    void testVoidIsVoidType(void)
+    {
+        VoidTypeHandle voidType;
+        TS_ASSERT(voidType.isVoidType());
+    }
 
-		void testVoidIsNotIntType(void)
-		{
-				VoidTypeHandle voidType;
-				TS_ASSERT(!voidType.isIntType())
-		}
+    void testVoidIsNotIntType(void)
+    {
+        VoidTypeHandle voidType;
+        TS_ASSERT(!voidType.isIntType())
+    }
 
-		void testVoidIsNotFloatType(void)
-		{
-				VoidTypeHandle voidType;
-				TS_ASSERT(!voidType.isFloatType())
-		}
+    void testVoidIsNotFloatType(void)
+    {
+        VoidTypeHandle voidType;
+        TS_ASSERT(!voidType.isFloatType())
+    }
 
-		void testVoidIsNotPointerType(void)
-		{
-				VoidTypeHandle voidType;
-				TS_ASSERT(!voidType.isPointerType())
-		}
+    void testVoidIsNotPointerType(void)
+    {
+        VoidTypeHandle voidType;
+        TS_ASSERT(!voidType.isPointerType())
+    }
 
-		void testVoidIsNotFunctionType(void)
-		{
-				VoidTypeHandle voidType;
-				TS_ASSERT(!voidType.isFunctionType())
-		}
+    void testVoidIsNotFunctionType(void)
+    {
+        VoidTypeHandle voidType;
+        TS_ASSERT(!voidType.isFunctionType())
+    }
 
-		void testVoidIsNotVectorType(void)
-		{
-				VoidTypeHandle voidType;
-				TS_ASSERT(!voidType.isVectorType())
-		}
+    void testVoidIsNotVectorType(void)
+    {
+        VoidTypeHandle voidType;
+        TS_ASSERT(!voidType.isVectorType())
+    }
 
-		void testVoidIsNotArrayType(void)
-		{
-				VoidTypeHandle voidType;
-				TS_ASSERT(!voidType.isArrayType())
-		}
+    void testVoidIsNotArrayType(void)
+    {
+        VoidTypeHandle voidType;
+        TS_ASSERT(!voidType.isArrayType())
+    }
 
-		void testVoidIsNotStructType(void)
-		{
-				VoidTypeHandle voidType;
-				TS_ASSERT(!voidType.isStructType())
-		}
+    void testVoidIsNotStructType(void)
+    {
+        VoidTypeHandle voidType;
+        TS_ASSERT(!voidType.isStructType())
+    }
 
-		void testVoidIsCompatibleWithVoid(void)
-		{
-				VoidTypeHandle voidType, anotherVoid;
-				TS_ASSERT(voidType.isCompatibleWith(&anotherVoid));
-		}
+    void testVoidIsCompatibleWithVoid(void)
+    {
+        VoidTypeHandle voidType, anotherVoid;
+        TS_ASSERT(voidType.isCompatibleWith(&anotherVoid));
+    }
 
-		void testVoidIsNotCompatibleWithOtherTypes(void)
-		{
-				VoidTypeHandle voidType;
-				IntTypeHandle intType(32);
-				FloatTypeHandle floatType(64);
-				TS_ASSERT(!voidType.isCompatibleWith(&intType));
-				TS_ASSERT(!voidType.isCompatibleWith(&floatType));
-		}
+    void testVoidIsNotCompatibleWithOtherTypes(void)
+    {
+        VoidTypeHandle voidType;
+        IntTypeHandle intType(32);
+        FloatTypeHandle floatType(64);
+        TS_ASSERT(!voidType.isCompatibleWith(&intType));
+        TS_ASSERT(!voidType.isCompatibleWith(&floatType));
+    }
 
-		void testVoidGetLLVMType(void)
-		{
-				TS_SKIP("TODO: LLVM tests");
-		}
+    void testVoidGetLLVMType(void)
+    {
+        TS_SKIP("TODO: LLVM tests");
+    }
 };
